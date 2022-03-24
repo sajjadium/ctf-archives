3df386b47296ddc181c982d558b6099adb2cea0b
Sajjad JJ Arshad <sajjadium@google.com>
2022-03-24 18:43:03 +0000

    OFPPT challs

---
+++ OFPPT/2022/crypto/XOR/honey.py
@@ -0,0 +1,22 @@
+import os
+
+FLAG = b'OFPPT-CTF{...REDACTED...}'
+
+
+class HoneyComb:
+    def __init__(self, key):
+        self.vals = [i for i in key]
+
+    def turn(self):
+        self.vals = [self.vals[-1]] + self.vals[:-1]
+
+    def encrypt(self, msg):
+        keystream = []
+        while len(keystream) < len(msg):
+            keystream += self.vals
+            self.turn()
+        return bytes([msg[i] ^ keystream[i] for i in range(len(msg))]).hex()
+
+hc = HoneyComb(os.urandom(6))
+
+print(hc.encrypt(FLAG))
